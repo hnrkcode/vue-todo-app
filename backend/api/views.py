8728abfd7a5781c2cd183cd28f5868d2febf7b07
Henrik Petersson <henrik@tutamail.com>
2020-02-23 13:37:05 +0100

    Create viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from . import serializers
+from . import models
 
-# Create your views here.
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = models.TodoModel.objects.all()
+    serializer_class = serializers.TodoSerializer
