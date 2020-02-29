1201e8e5ef90264b8095e3869d3665c1d427bae2
Henrik Petersson <henrik@tutamail.com>
2020-02-29 14:07:50 +0100

    Add font awesome icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
 import "bulma/css/bulma.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faEdit, faTrashAlt);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
