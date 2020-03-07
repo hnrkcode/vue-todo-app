2c2eab112819dd8bbc2282fd502d35191a956aae
Henrik Petersson <henrik@tutamail.com>
2020-03-07 16:54:28 +0100

    Refactor store
    Create separate module for CRUD operations
    and create a custom axios instance.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,115 +1,15 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from "axios";
+import crud from "./modules/crud";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    todos: [],
-    limit: {
-      header: false,
-      body: false
-    }
-  },
-  getters: {
-    todoCount: state => {
-      return state.todos.length;
-    },
-    todoList: state => {
-      return state.todos;
-    },
-    todoLimit: state => {
-      return state.limit;
-    }
-  },
-  mutations: {
-    createTodo(state, payload) {
-      // Add new data at the beginning of the array.
-      state.todos.splice(0, 0, payload.data);
-    },
-    deleteTodo(state, payload) {
-      for (let i in state.todos) {
-        if (state.todos[i].id === payload) {
-          state.todos.splice(i, 1);
-          break;
-        }
-      }
-    },
-    editTodo(state, payload) {
-      for (let i in state.todos) {
-        if (state.todos[i].id === payload.id) {
-          state.todos[i].name = payload.header;
-          state.todos[i].text = payload.body;
-          break;
-        }
-      }
-    },
-    fetchTodos(state, payload) {
-      state.todos = payload;
-    },
-    setLimitStatus(state, payload) {
-      state.limit.header = payload.name;
-      state.limit.body = payload.text;
-    }
-  },
-  actions: {
-    createTodo(context, payload) {
-      axios
-        .post("http://127.0.0.1:8000/api/todos/", {
-          name: payload.header,
-          text: payload.body
-        })
-        .then(response => {
-          context.commit("createTodo", response);
-        })
-        .then(() => {
-          // Reset limit status.
-          context.commit("setLimitStatus", {
-            header: false,
-            body: false
-          });
-        })
-        .catch(({ response: { data } }) => {
-          context.commit("setLimitStatus", {
-            name: data.name ? true : false,
-            text: data.text ? true : false
-          });
-        });
-    },
-    deleteTodo(context, payload) {
-      axios
-        .delete("http://127.0.0.1:8000/api/todos/" + payload + "/")
-        .then(() => {
-          context.commit("deleteTodo", payload);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    editTodo(context, payload) {
-      axios
-        .put("http://127.0.0.1:8000/api/todos/" + payload.id + "/", {
-          name: payload.header,
-          text: payload.body
-        })
-        .then(() => {
-          context.commit("editTodo", payload);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    },
-    fetchTodos(context) {
-      axios
-        .get("http://127.0.0.1:8000/api/todos/")
-        .then(response => {
-          context.commit("fetchTodos", response.data);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
-  },
-  modules: {}
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    crud
+  }
 });
---
+++ frontend/src/store/modules/crud.js
@@ -0,0 +1,119 @@
+import axios from "@/http/custom-axios";
+
+const state = {
+  todos: [],
+  limit: {
+    header: false,
+    body: false
+  }
+};
+
+const getters = {
+  todoCount: state => {
+    return state.todos.length;
+  },
+  todoList: state => {
+    return state.todos;
+  },
+  todoLimit: state => {
+    return state.limit;
+  }
+};
+
+const mutations = {
+  createTodo(state, payload) {
+    // Add new data at the beginning of the array.
+    state.todos.splice(0, 0, payload.data);
+  },
+  deleteTodo(state, payload) {
+    for (let i in state.todos) {
+      if (state.todos[i].id === payload) {
+        state.todos.splice(i, 1);
+        break;
+      }
+    }
+  },
+  editTodo(state, payload) {
+    for (let i in state.todos) {
+      if (state.todos[i].id === payload.id) {
+        state.todos[i].name = payload.header;
+        state.todos[i].text = payload.body;
+        break;
+      }
+    }
+  },
+  fetchTodos(state, payload) {
+    state.todos = payload;
+  },
+  setLimitStatus(state, payload) {
+    state.limit.header = payload.name;
+    state.limit.body = payload.text;
+  }
+};
+
+const actions = {
+  createTodo(context, payload) {
+    axios
+      .post("/api/todos/", {
+        name: payload.header,
+        text: payload.body
+      })
+      .then(response => {
+        console.log(response);
+        context.commit("createTodo", response);
+      })
+      .then(() => {
+        // Reset limit status.
+        context.commit("setLimitStatus", {
+          header: false,
+          body: false
+        });
+      })
+      .catch(({ response: { data } }) => {
+        context.commit("setLimitStatus", {
+          name: data.name ? true : false,
+          text: data.text ? true : false
+        });
+      });
+  },
+  deleteTodo(context, payload) {
+    axios
+      .delete("/api/todos/" + payload + "/")
+      .then(() => {
+        context.commit("deleteTodo", payload);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  editTodo(context, payload) {
+    axios
+      .put("/api/todos/" + payload.id + "/", {
+        name: payload.header,
+        text: payload.body
+      })
+      .then(() => {
+        context.commit("editTodo", payload);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  fetchTodos(context) {
+    axios
+      .get("/api/todos/")
+      .then(response => {
+        context.commit("fetchTodos", response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+};
+
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};
