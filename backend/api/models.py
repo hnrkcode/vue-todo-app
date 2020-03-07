e6937d8cebe919221ed6f6befc4f3cecae8371ad
Henrik Petersson <henrik@tutamail.com>
2020-03-07 11:59:15 +0100

    Set max length to text field

--- backend/api/models.py
+++ backend/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class TodoModel(models.Model):
     name = models.CharField(max_length=255)
-    text = models.TextField()
+    text = models.TextField(max_length=1000)
     created = models.DateField(auto_now_add=True)
 
     def __str__(self):
